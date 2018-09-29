1a648d5341694341c4a7b6f710b5a91fb7370d78
Qiao Zhu <zqa0@vip.qq.com>
2018-09-29 03:28:26 -0500

    Initial Commit

---
+++ AlgoDesign/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AlgoDesign
+//
+//  Created by Qiao Zhu on 9/29/18.
+//  Copyright © 2018 Qiao Zhu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
