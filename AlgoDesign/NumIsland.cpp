4eab15743c21e93d3f4580c990ca2ed0971d422a
Qiao Zhu <zqa0@vip.qq.com>
2018-09-29 05:48:04 -0500

    Number of Islands

---
+++ AlgoDesign/NumIsland.cpp
@@ -0,0 +1,14 @@
+//
+//  NumIsland.cpp
+//  AlgoDesign
+//
+//  Created by Qiao Zhu on 9/29/18.
+//  Copyright © 2018 Qiao Zhu. All rights reserved.
+//
+
+#include <vector>
+using namespace std;
+
+int numIslands(vector<vector<char>>& grid) {
+    return 0;
+}
